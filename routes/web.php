33170cade1c64818f4522e4a1ed65e91fd6a0501
nevercom <nevercom@gmail.com>
2024-08-19 13:53:17 +0330

    register routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\WebsiteController;
+use App\Http\Controllers\SubscriptionController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// using web routes for simplicity, should be api routes in a real-world scenario (artisan install:api)
+
+Route::get('/websites', [WebsiteController::class, 'index']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::post('/subscribe', [SubscriptionController::class, 'store']); // since there is no authentication, we don't use Route Model Binding
