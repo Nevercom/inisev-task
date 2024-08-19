2508b57eceac78ca8981ed6ec6c06c880a729cab
nevercom <nevercom@gmail.com>
2024-08-19 13:42:44 +0330

    create a list of pending notifications when a new post is published

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Models\PendingPostNotification;
 
 class PostController extends Controller
 {
@@ -17,6 +18,15 @@ class PostController extends Controller
 
         $post = Post::create($data);
 
+        // create a list of notifications to be processed later
+        $subscribers = $post->website->subscribers->map(function ($subscriber) use ($post) {
+            return [
+                'user_id' => $subscriber->user_id,
+                'post_id' => $post->id,
+            ];
+        });
+
+        PendingPostNotification::create($subscribers);
 
         return response()->json(
             [
