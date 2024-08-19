79eff32b6a777e3c0793592351e69428e92d101b
nevercom <nevercom@gmail.com>
2024-08-19 14:02:52 +0330

    use caching

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\Website;
+use Illuminate\Support\Facades\Cache;
 
 class WebsiteController extends Controller
 {
     public function index()
     {
-        return response()->json([
-            'websites' => Website::all(),
+        return response()->json(data: [
+            'websites' => Cache::remember(
+                key: 'websites',
+                ttl: 3600, // cache for 1 hour, should use invalidation strategy in production
+                callback: fn() => Website::all()
+            ),
         ]);
     }
 }
