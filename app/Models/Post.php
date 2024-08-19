b89ac92ea659f1964cfaaa03cf05309cd73ee758
nevercom <nevercom@gmail.com>
2024-08-19 13:41:22 +0330

    create relationships

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['website_id', 'title', 'description'];
+
+    public function website(): BelongsTo
+    {
+        return $this->belongsTo(Website::class);
+    }
 }
--- app/Models/Website.php
+++ app/Models/Website.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Website extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'url'];
+
+    public function subscribers(): HasMany
+    {
+        return $this->hasMany(Subscription::class);
+    }
 }
